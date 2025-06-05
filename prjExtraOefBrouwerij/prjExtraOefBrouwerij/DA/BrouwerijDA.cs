7426f18bc8cb4ff1f689a50b57838b92afcba9cc
Luka Verbrugghe <verbruggheluka0@gmail.com>
2025-06-05 11:18:10 +0200

    Inladen alle brouwerijen

--- prjExtraOefBrouwerij/prjExtraOefBrouwerij/DA/BrouwerijDA.cs
+++ prjExtraOefBrouwerij/prjExtraOefBrouwerij/DA/BrouwerijDA.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using prjExtraOefBrouwerij.Helper;
+using prjExtraOefBrouwerij.Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +13,40 @@ namespace prjExtraOefBrouwerij.DA
 {
     public class BrouwerijDA
     {
+        public static List<Brouwerij> ophalenBrouwerijen()
+        {
+            List<Brouwerij> brouwerijen = new List<Brouwerij>();
+
+            string sql = "SELECT id,Name,Email,Website,ImagesURL,Description_nl,Description_en FROM brouwerij";
+
+            MySqlConnection conn = Database.MaakVerbinding();
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.CommandType = System.Data.CommandType.Text;
+
+            //geen parameters
+
+            MySqlDataReader r = cmd.ExecuteReader();
+
+            while (r.Read())
+            {
+                brouwerijen.Add(Create(r));
+            }
+
+            return brouwerijen;
+        }
+
+        public static Brouwerij Create(IDataRecord record)
+        {
+            return new Brouwerij()
+            {
+                id = Convert.ToInt32(record["id"]),
+                Name = record["Name"].ToString(),
+                Email = record["Email"].ToString(),
+                Website = record["Website"].ToString(),
+                ImagesURL = record["ImagesURL"].ToString(),
+                Description_nl = record["Description_nl"].ToString(),
+                Description_en = record["Description_en"].ToString()
+            };
+        }
     }
 }
--- prjExtraOefBrouwerij/prjExtraOefBrouwerij/Form1.cs
+++ prjExtraOefBrouwerij/prjExtraOefBrouwerij/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using prjExtraOefBrouwerij.DA;
+using prjExtraOefBrouwerij.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +21,13 @@ namespace prjExtraOefBrouwerij
 
         private void btnToonData_Click(object sender, EventArgs e)
         {
-
+            lsvBrouwerijen.Items.Clear();
+            foreach(Brouwerij b in BrouwerijDA.ophalenBrouwerijen())
+            {
+                ListViewItem item = new ListViewItem(new string[] {b.id.ToString(), b.Name, b.Email, b.Website });
+                item.Tag = b;
+                lsvBrouwerijen.Items.Add(item);
+            }
         }
     }
 }
